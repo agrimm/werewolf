043c7a671127ac52ea2fe938f54163e50dfd172a
Chris Lloyd <christopher.lloyd@gmail.com>
2009-08-10 13:26:38 +1000

    Third draft.

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,32 @@
 (function($){
     $(function(){
 
-        $(document.body).grid();
+        $('.centify').grid();
 
+        var flourish = $('#flourish'),
+            nSleeps = $('#flourish time'),
+            nSleepsMsg = $('#flourish time .units');
+
+        var start = {
+                opacity: 0,
+                paddingTop: nSleeps.css('paddingTop')
+            },
+            end = {
+                opacity: nSleepsMsg.css('opacity'),
+                paddingTop: '10px' // start.paddingTop - 5
+            },
+            animDur = 300;
+
+        console.log(start);
+        console.log(end);
+
+        flourish.mouseenter(function(){
+            nSleepsMsg.css('opacity', start.opacity);
+            nSleeps.animate({paddingTop: end.paddingTop}, animDur);
+            nSleepsMsg.show().animate({opacity: end.opacity}, animDur);
+        }).mouseleave(function(){
+            nSleeps.animate({paddingTop: start.paddingTop}, animDur);
+            nSleepsMsg.animate({opacity: start.opacity}, animDur, function(){ nSleepsMsg.hide();});
+        });
     });
 })(jQuery);
--- werewolf.rb
+++ werewolf.rb
@@ -1,3 +1,28 @@
 # Tyrone will care the shit out of you
 gem 'tyrone', '0.3.2'
 require 'tyrone/app'
+
+gem 'astro_moon', '0.2'
+require 'astro/moon'
+
+set :phases => ['full', 'waning-gibbous', 'last-quarter', 'waning-crescent',
+  'new', 'waxing-crescent', 'first-quarter', 'waxing-gibbous', 'full']
+
+helpers do
+
+  def current_phase
+    'full'
+    # illum = Astro::Moon.phase.illumination
+    # case illum
+    #   when 0: 'full';
+    #   when 0...0.25: 'waning-gibbous';
+    #   when 0.25: 'last-quarter';
+    #   when 0.25...0.5: 'waning-crescent';
+    #   when 0.5: 'new';
+    #   when 0.5...0.75: 'waxing-crescent';
+    #   when 0.75: 'first-quarter';
+    #   when 0.75...1: 'waxing-gibbous';
+    #   else 'full';
+    # end
+  end
+end
