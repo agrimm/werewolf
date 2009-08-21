e950b3e1f70363fc2af9b43d944cd8f2022e949f
Chris Lloyd <christopher.lloyd@gmail.com>
2009-08-22 08:43:51 +1000

    Converted from Tyrone to a full app.
    Removed alot of the Astro code because it was very buggy.

--- werewolf.rb
+++ werewolf.rb
@@ -1,32 +1,10 @@
-# Tyrone will care the shit out of you
-gem 'tyrone', '0.3.2'
-require 'tyrone/app'
-
 gem 'activesupport', '2.3.3'
 require 'activesupport' # TODO Don't require everything
-
-gem 'astro_moon', '0.2'
-require 'astro/moon'
-
-set :phases => ['full', 'waning-gibbous', 'last-quarter', 'waning-crescent',
-  'new', 'waxing-crescent', 'first-quarter', 'waxing-gibbous', 'full']
 
 helpers do
 
   def current_phase
-    # 'full'
-    illum = Astro::Moon.phase.illumination
-    case illum
-      when 0: 'full';
-      when 0...0.25: 'waning-gibbous';
-      when 0.25: 'last-quarter';
-      when 0.25...0.5: 'waning-crescent';
-      when 0.5: 'new';
-      when 0.5...0.75: 'waxing-crescent';
-      when 0.75: 'first-quarter';
-      when 0.75...1: 'waxing-gibbous';
-      else 'full';
-    end
+    'full'
   end
 
   def full_moon?
@@ -34,12 +12,26 @@ helpers do
   end
 
   def next_full_moon
-    first_day_of_next_cycle = Astro::Moon.phasehunt.moon_end.tomorrow
-    Astro::Moon.phasehunt(first_day_of_next_cycle).moon_full
+    Date.civil(2009,9,5)
   end
 
   def days_until_full_moon
-    (next_full_moon - DateTime.now).floor + 1
+    (next_full_moon - DateTime.now).to_i
   end
 
+  def hidden
+    {:style => 'display:none'}
+  end
+
+end
+
+get '/' do
+  haml :home
+end
+
+get /^\/css\/(.+)\.css$/ do |style_file|
+  sass_file = File.join('public','css',"#{style_file}.sass")
+  pass unless File.exist?(sass_file)
+  content_type :css
+  sass File.read(sass_file)
 end
