39c8b3d94e295177aab5a9f08bc1d191cbe7a52f
Chris Lloyd <christopher.lloyd@gmail.com>
2009-09-07 11:11:10 +1000

    Fixed stupid JS bug.

--- public/js/application.js
+++ public/js/application.js
@@ -3,31 +3,23 @@
 
         var flourish = $('#flourish'),
             nSleeps = $('#flourish time'),
-            nSleepsMsg = $('#flourish time .units');
-
-        var start = {
-                opacity: 0,
-                paddingTop: nSleeps.css('paddingTop')
-            },
-            end = {
-                opacity: nSleepsMsg.css('opacity'),
-                paddingTop: '10px' // start.paddingTop - 5
-            },
+            nSleepsMsg = $('#flourish time .units'),
+            
+            startPaddingTop = nSleeps.css('paddingTop'),
             animDur = 300;
-
+        
         flourish.mouseenter(function(){
-            nSleepsMsg.css('opacity', start.opacity);
-            nSleeps.animate({paddingTop: end.paddingTop}, animDur);
-            nSleepsMsg.show().animate({opacity: end.opacity}, animDur);
+            nSleepsMsg.fadeIn(animDur);
+            nSleeps.animate({paddingTop: '10px'}, animDur);
         }).mouseleave(function(){
-            nSleeps.animate({paddingTop: start.paddingTop}, animDur);
-            nSleepsMsg.animate({opacity: start.opacity}, animDur, function(){ nSleepsMsg.hide();});
+            nSleeps.animate({paddingTop: startPaddingTop}, animDur);
+            nSleepsMsg.fadeOut(animDur);
         });
 
         var flickr = $('#flickr');
 
         $.getJSON('http://api.flickr.com/services/feeds/groups_pool.gne? id=1225246@N22&lang=en-us&format=json&jsoncallback=?', function(data){
-            flickr.children('.loading').fadeOut();
+            flickr.children('.loading').hide();
             $.each(data.items, function(i,item){
                 if (i >= 5) { return };
 
